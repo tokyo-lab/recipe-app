9531c68e3fa5b3b07796259ab0b5fc3b63c6870d
tokyo <your-email@example.comyour-email@example.com>
2023-12-14 19:54:41 -0500

    wip recipe

---
+++ recipe.py
@@ -0,0 +1,12 @@
+import os
+
+category_path = 'categories'
+
+def choose_category(categories):
+    category = input("Select a category")
+    return category
+
+
+def create_category():
+    category = input("Select a category")
+    if
