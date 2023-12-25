4f27afde612c8dc5eb1c2392a0d7cc9724fd7514
tokyo <tokyolabs@protonmail.com>
2023-12-25 18:16:38 -0500

    select action completed

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ from utilities import get_actions
 # import recipe
 
 
-def get_action_name(action_number):
+def get_action_name():
+    action_number = input("Select an action: ")
     actions_dict = get_actions()
+
+    try:
+        action_number = int(action_number)
+    except ValueError:
+        return "Invalid input. Enter a number"
     action_name = actions_dict.get(action_number, "Key not found")
     return action_name
 
 
-user_action = input("Select an action: ")
-print(type(user_action))
-action_name = get_action_name(user_action)
-print("user_action", user_action)
+# user_action = input("Select an action: ")
+# print(type(user_action))
+action_name = get_action_name()
 print("action_number", action_name)
 
 # if action_number is not None:
