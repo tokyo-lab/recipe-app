091ef50f7e73b484d771ef5878217c8517ce05da
tokyo <tokyolabs@protonmail.com>
2023-12-28 09:41:41 -0500

    finihsed app

--- app.py
+++ app.py
@@ -30,11 +30,13 @@ while True:
         selected_category = recipe_category.get_category()
         print("selected category -> ", selected_category)
         recipe.get_recipe(selected_category)
+
     # Create recipe
     elif selected_action_number == 2:
         selected_category = recipe_category.get_category()
         print("selected category -> ", selected_category)
         recipe.create_recipe(selected_category)
+
     # Delete recipe
     elif selected_action_number == 3:
         print("Here are all the categories. Pick one: ")
@@ -44,5 +46,16 @@ while True:
         selected_recipe = recipe.get_recipe(selected_category[1])
         print("selected_recipe -> ", selected_recipe)
         recipe.delete_recipe(selected_category, selected_recipe)
+
+    # Create recipe category
+    elif selected_action_number == 4:
+        recipe_category.create_category()
+
+    # Delete recipe category
+    elif selected_action_number == 5:
+        recipe_category.delete_category()
+    # End program
+    elif selected_action_number == 6:
+        exit()
     else:
         print("Invalid selection. Please select a valid number. ")
